a7220015b4a2bfe1f65bacabbcf254f132832504
Kevin Patel <90125092+pateruu@users.noreply.github.com>
2024-01-02 15:10:38 -0500

    Added clear screen function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include "main.h"
 #include <cmath>
+#include <cstdlib>
 
 main(){
 
-
+  
+  
 
   return 0;
+}
+
+void clearScreen(){
+
+  //Clear command from system function library to clear the console when called.
+  system("clear");
+}
+
+
+void mainmenu(){
+
+  std::cout << "Choose a calculator:" << std::endl;
+
+
+  
 }
