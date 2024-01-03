d8226d90c32bb409f839293fe518cc9db1bc8ed1
Kevin Patel <90125092+pateruu@users.noreply.github.com>
2024-01-02 20:49:58 -0500

    Finished the BMI calculator for the program.

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ main(){
 
       clearConsole();
       menuOptions[choice - 1].second();
-      std::cout << "\n\n";
+      std::cout << "\n";
 
       //Otherwise tell user that the choice is invalid.
     } else{
@@ -72,8 +72,126 @@ void mainMenu(const std::vector<std::pair<std::string, std::function<void()>>>& 
 
 void bodyMassIndex(){
 
-  std::cout << "bodyMassIndex";
+  double feet;
+  double inches;
+  double lbs;
+  double cm;
+  double kg;
+  int choice;
+  double BMI;
 
+
+  while(true){
+    
+    std::cout << "Body Mass Index (BMI) Menu:" << std::endl;
+    std::cout << "0. Exit" << std::endl;
+    std::cout << "1. Imperial System" << std::endl;
+    std::cout << "2. Metric System" << std::endl;
+    std::cout << "___________________________\n\n";
+    std::cout << "Which system of measurement would you like to use?" << std::endl;
+    std::cin >> choice;
+
+    if(choice == 0){
+      
+      clearConsole();
+      break;
+
+    } else if(choice == 1){
+      
+      clearConsole();
+
+      //Ask user for details.
+      std::cout << "Height:" << std::endl;
+
+      std::cout << "Please enter feet:" << std::endl;
+      std::cin >> feet;
+
+      std::cout << "Please enter inches:" << std::endl;
+      std::cin >> inches;
+      clearConsole();
+
+      std::cout << "Weight:" << std::endl;
+      std::cout << "Please enter pounds:" << std::endl;
+      std::cin >> lbs;
+      clearConsole();
+      
+      //Calculations
+      BMI = 703 * ( lbs / (((feet*12) + inches) * ((feet*12) + inches)));
+
+      //output statements
+      std::cout << "You BMI is: " << BMI << std::endl;
+      
+      if(BMI < 18.5){
+
+        std::cout << "You are considered: Underweight (<18.5)" << std::endl;
+        std::cout << "_____________________________________________\n\n" << std::endl;
+
+      } else if((BMI >= 18.5) && (BMI <= 24.9)){
+
+        std::cout << "You are considered: Normal Weight (18.5–24.9)" << std::endl;
+        std::cout << "_____________________________________________\n\n" << std::endl;
+
+      } else if((BMI >= 25) && (BMI <= 29.9)){
+
+        std::cout << "You are considered: Overweight (25–29.9)" << std::endl;
+        std::cout << "_____________________________________________\n\n" << std::endl;
+
+      } else{
+
+        std::cout << "You are considered: Obese (>30)" << std::endl;
+        std::cout << "_____________________________________________\n\n" << std::endl;
+      }
+
+
+    } else if (choice == 2){
+      
+      clearConsole();
+
+      //Ask user for details.
+      std::cout << "Height:" << std::endl;
+      std::cout << "Please enter centimeters:" << std::endl;
+      std::cin >> cm;
+      clearConsole();
+
+      std::cout << "Weight:" << std::endl;
+      std::cout << "Please enter kilograms:" << std::endl;
+      std::cin >> kg;
+      clearConsole();
+      
+      //Calculations
+      BMI = kg/((cm / 100) * (cm / 100));
+
+      //output statements
+      std::cout << "You BMI is: " << BMI << std::endl;
+      
+      if(BMI < 18.5){
+
+        std::cout << "You are considered: Underweight (<18.5)" << std::endl;
+        std::cout << "_____________________________________________\n\n" << std::endl;
+
+      } else if((BMI >= 18.5) && (BMI <= 24.9)){
+
+        std::cout << "You are considered: Normal Weight (18.5-24.9)" << std::endl;
+        std::cout << "_____________________________________________\n\n" << std::endl;
+
+      } else if((BMI >= 25) && (BMI <= 29.9)){
+
+        std::cout << "You are considered: Overweight (25-29.9)" << std::endl;
+        std::cout << "_____________________________________________\n\n" << std::endl;
+
+      } else{
+
+        std::cout << "You are considered: Obese (>30)" << std::endl;
+        std::cout << "_____________________________________________\n\n" << std::endl;
+      }
+
+    } else{
+      
+      clearConsole();
+      std::cout << "INVALID CHOICE" << std::endl;
+      std::cout << "______________\n\n" << std::endl;
+    }
+  }
 }
 
 void basalMetabolicRate(){
